be96ec3397f85d73779bd624786c6c553cda1a5f
Aryan-Rathour <aryanrathour066@gmail.com>
2025-05-08 23:47:56 +0530

    ...

--- hooks/api/getAllConversation.js
+++ hooks/api/getAllConversation.js
@@ -2,10 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 
 const getAllConversation = async(userId)=>{
 
-    const userData = JSON.parse(localStorage.getItem('user'));
-  console.log("userIdgggg", userData.user._id);
-  const userId = userData?.user?._id;
-
     const response = await fetch(`https://api.bizwapp.com/api/auth/conversationHistory/${userId}`)
     if(!response.ok){
         throw new Error('Failed to get Conversation.')
