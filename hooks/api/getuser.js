c0db3c40bace01ec776b4c95b1e598de8c3a6207
Aryan-Rathour <aryanrathour066@gmail.com>
2025-06-22 00:03:24 +0530

    fix: update fetchUser API URL to correct endpoint

--- hooks/api/getuser.js
+++ hooks/api/getuser.js
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 
 
 const fetchUser = async () => {
-  const res = await fetch('https://api.bizwapp.com/api/auth/me', {
+  const res = await fetch('https://bizwapp-backend-2.onrender.com/api/auth/me', {
     method: 'GET',
     credentials: 'include', // sends cookies (important)
   });
