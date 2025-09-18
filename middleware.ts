e906fa2049b93b6744b4dff9837f49a22fc8c4bf
Aryan rathour <aryanrathour066@gmail.com>
2025-09-18 16:04:08 +0530

    remove middleware

--- middleware.ts
+++ middleware.ts
@@ -5,44 +5,44 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   console.log("middlewarekhbkhb");
   
-  const token = request.cookies.get("token")?.value; // cookie से token निकालना
+  // const token = request.cookies.get("token")?.value; // cookie से token निकालना
 
-  console.log("token", token);
+  // console.log("token", token);
 
-  console.log("All Cookies in middleware:", request.cookies.getAll());
+  // console.log("All Cookies in middleware:", request.cookies.getAll());
   
 
-  const publicPaths = [
-    '/login',
-    '/signup',
-    '/_next',
-    '/favicon.ico',
-    '/robots.txt',
-    '/manifest.json',
-    '/images',
-    '/fonts',
-    '/icons',
-  ]
+  // const publicPaths = [
+  //   '/login',
+  //   '/signup',
+  //   '/_next',
+  //   '/favicon.ico',
+  //   '/robots.txt',
+  //   '/manifest.json',
+  //   '/images',
+  //   '/fonts',
+  //   '/icons',
+  // ]
 
-  const path = request.nextUrl.pathname
+  // const path = request.nextUrl.pathname
 
-  const isPublicPath = publicPaths.some((publicPath) =>
-    path.startsWith(publicPath)
-  )
+  // const isPublicPath = publicPaths.some((publicPath) =>
+  //   path.startsWith(publicPath)
+  // )
 
-  // ✅ अगर path private है और token नहीं है → redirect to /login
-  if (!token && !isPublicPath) {
-    const loginUrl = new URL('/login', request.url) // same domain पर login
-    return NextResponse.redirect(loginUrl)
-  }
+  // // ✅ अगर path private है और token नहीं है → redirect to /login
+  // if (!token && !isPublicPath) {
+  //   const loginUrl = new URL('/login', request.url) // same domain पर login
+  //   return NextResponse.redirect(loginUrl)
+  // }
 
-  // ✅ अगर पहले से logged in है और वो /login या /signup पर जाने की कोशिश कर रहा है → redirect to home/dashboard
-  if (token && (path.startsWith('/login') || path.startsWith('/signup'))) {
-    const homeUrl = new URL('/', request.url)
-    return NextResponse.redirect(homeUrl)
-  }
+  // // ✅ अगर पहले से logged in है और वो /login या /signup पर जाने की कोशिश कर रहा है → redirect to home/dashboard
+  // if (token && (path.startsWith('/login') || path.startsWith('/signup'))) {
+  //   const homeUrl = new URL('/', request.url)
+  //   return NextResponse.redirect(homeUrl)
+  // }
 
-  return NextResponse.next()
+  // return NextResponse.next()
 }
 
 export const config = {
